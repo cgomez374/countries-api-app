fa556c8e603c16893c80556f2ca4c90567b44fd7
Carlos Gomez <cgomez374@yahoo.com>
2021-09-11 13:41:52 -0700

    added region filter funtionality and styling

--- src/components/inputContainer/inputContainer.js
+++ src/components/inputContainer/inputContainer.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getCountriesByName } from '../../contexts/api/getCountriesByName';
+import {getCountriesByRegions} from '../../contexts/api/getCountriesByRegions';
+import {getCountries} from '../../contexts/api/getCountries'
 import Styles from './inputContainer.module.css'
 
 const InputContainer = ({ setCountries, setError }) => {
@@ -16,16 +18,27 @@ const InputContainer = ({ setCountries, setError }) => {
     //update countries as input changes
     useEffect(() => {
         getCountriesByName(setCountries, setError, input);
-    }, [input])
+    }, [input]);
+
+    const setCountriesByRegion = (e) => {
+        if(e !== null && e.target.value !== "Filter by Region") {
+            getCountriesByRegions(setCountries, setError, e.target.value);
+        }
+        else if(e.target.value === "Filter by Region"){
+            getCountries(setCountries, setError);
+        }
+    }
 
     return (
         <article className={Styles.filter}>
             <input type="text" onChange={changeHandler} className={Styles.search} placeholder='Search for a country...' />
-            <select name="cars" id="cars">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option>
+            <select name="regions" onChange={setCountriesByRegion} className={Styles.select}>
+                <option value="Filter by Region">Filter by Region</option>
+                <option value="Africa">Africa</option>
+                <option value="Americas">Americas</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
                 </select>
         </article>
     )
---
+++ src/contexts/api/getCountriesByRegions.js
@@ -0,0 +1,13 @@
+export const getCountriesByRegions = async (setCountries, setError, region = '') => {
+    if(region !== ''){
+        const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`);
+        var data = await response.json();
+        if(data){
+            console.log(data)
+            setCountries(data);
+        }
+        else{
+            setError('Could not retrieve countries')
+        }
+    }
+}
